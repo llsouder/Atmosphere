8ed710334aa3814e92a93a9c896fd5231f789eff
Lonnie Souder <lonnie@souderfamily.net>
2018-05-05 10:54:46 -0400

    Implement standard temperature calculation.
    Also add feet to meters and fahrenheit to celsius conversion.

--- src/atmosphere/core.clj
+++ src/atmosphere/core.clj
@@ -1,6 +1,23 @@
 (ns atmosphere.core
   (:gen-class))
 
+(defn f-to-c
+  "Convert from fahrenheit to celsius."
+  [f]
+  (/ (- f 32.0) 1.8))
+
+(defn f-to-m
+  "Convert from feet to meters."
+  [f]
+  (* f 0.3048))
+
+(defn std-temp
+  "Calculate standard temperature.
+  Results are within .1 of the data
+  found on http://meteorologytraining.tpub.com/14269/css/14269_75.htm."
+  [h]
+  (- 15.04 (* 0.00649 h)))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
--- test/atmosphere/core_test.clj
+++ test/atmosphere/core_test.clj
@@ -2,6 +2,27 @@
   (:require [clojure.test :refer :all]
             [atmosphere.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(defn difference ^double [^double x ^double y]
+  (Math/abs (double (- x y))))
+
+(defn close? [tolerance x y]
+  (< (difference x y) tolerance))
+
+(deftest convert-temp-test
+  (testing "Convert from fahrenheit to celsius."
+    (is (= 0.0 (f-to-c 32.0)))
+    (is (= 100.0 (f-to-c 212.0)))
+    (is (close? 0.1 -22.6 (f-to-c -8.8)))
+    (is (= 15.0 (f-to-c 59.0)))))
+
+(deftest convert-altitude-test
+  (testing "Convert from feet to meters."
+    (is (= 0.0 (f-to-m 0.0)))
+    (is (= 0.3048 (f-to-m 1.0)))
+    (is (= 3048.0 (f-to-m 10000)))))
+
+(deftest std-temp-test
+  (testing "Calculating standard temperature calculation."
+    (is (close? 0.1 15.0 (std-temp 0)))
+    (is (close? 0.1 -4.8 (std-temp (f-to-m 10000))))
+    (is (close? 0.1 -24.6 (std-temp (f-to-m 20000))))))
