9dc27c16bb65e2b0d4bb71bd84c772b61c41319b
Lonnie Souder <lonnie@souderfamily.net>
2018-05-05 10:02:01 -0400

    Initialize as a basic clojure app.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to atmosphere
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/atmosphere/core_test.clj
@@ -0,0 +1,7 @@
+(ns atmosphere.core-test
+  (:require [clojure.test :refer :all]
+            [atmosphere.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
